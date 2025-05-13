4b35ad382927c840a1d35339a983b0a3411a5e2f
Tom <thomas.marsh@babcockdev.onmicrosoft.com>
2025-05-13 11:42:42 +0100

    - added orbit camera around tank

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,16 +1,24 @@
-use bevy::{color::palettes::css::{GREEN, BLUE}, prelude::*};
-use bevy_rapier3d::prelude::*;
+use bevy::{color::palettes::css::{BLUE}, prelude::*, pbr::CascadeShadowConfigBuilder};
+use bevy_rapier3d::{prelude::*};
+use std::f32::consts::PI;
+use bevy_panorbit_camera::PanOrbitCameraPlugin;
+use bevy_panorbit_camera::PanOrbitCamera;
 
 fn main() {
     App::new()
     .add_plugins((
         DefaultPlugins,
         RapierPhysicsPlugin::<NoUserData>::default(),
-        RapierDebugRenderPlugin::default()
+        PanOrbitCameraPlugin
+        //RapierDebugRenderPlugin::default()
     ))
     .add_systems(Startup, setup)
-    .add_systems(Update, tank_movement)
-    //.add_systems(Update, camera_follow)
+    .add_systems(Update, (
+        tank_movement,
+        shoot_projectile,
+        move_projectile,
+        camera_follow, 
+    ))
     .run();
 }
 
@@ -18,7 +26,20 @@ fn main() {
 struct Tank;
 
 #[derive(Component)]
-struct Camera;
+struct Camera {
+    use_follow_cam: bool,
+}
+
+impl Camera {
+    fn default() -> Self {
+        Self {
+            use_follow_cam: true,
+        }
+    }
+}
+
+#[derive(Component)]
+struct Projectile;
 
 fn setup(
     mut commands: Commands,
@@ -28,75 +49,49 @@ fn setup(
 ) {
 
     // Light
-    commands.spawn((
-        PointLight {
-            intensity: 2000.0,
-            shadows_enabled: true,
-            ..default()},
-        Transform::from_xyz(4.0, 8.0, 4.0),
-        GlobalTransform::default()));
+    // commands.spawn((
+    //     PointLight {
+    //         intensity: 4000.0,
+    //         shadows_enabled: true,
+    //         color: WHITE.into(),
+    //         ..default()},
+    //     Transform::from_xyz(0.0, 20.0, 0.0)));
+
+        commands.spawn((
+            DirectionalLight {
+                illuminance: light_consts::lux::OVERCAST_DAY,
+                shadows_enabled: true,
+                ..default()
+            },
+            Transform {
+                translation: Vec3::new(0.0, 2.0, 0.0),
+                rotation: Quat::from_rotation_x(-PI / 4.),
+                ..default()
+            },
+            // The default cascade config is designed to handle large scenes.
+            // As this example has a much smaller world, we can tighten the shadow
+            // bounds for better visual quality.
+            CascadeShadowConfigBuilder {
+                first_cascade_far_bound: 4.0,
+                maximum_distance: 10.0,
+                ..default()
+            }
+            .build(),
+        ));
 
     //Ground Plane
     commands.spawn((
         Mesh3d(meshes.add(Mesh::from(Plane3d::default().mesh().size(20.0,20.0).subdivisions(10)))),
-        MeshMaterial3d(materials.add(StandardMaterial {base_color: GREEN.into(), ..Default::default()})),
+        MeshMaterial3d(materials.add(StandardMaterial {base_color_texture: Some(asset_server.load("coast_sand_rocks_02_2k/textures/coast_sand_rocks_02_diff_2k.jpg")), ..Default::default()})),
         Collider::cuboid(10.0, 0.1, 10.0)
     ));
 
-    //Tank Block
-    // let camo_texture = asset_server.load("camo.jpg");
-
-    // let body_entity = commands.spawn((
-    //     Mesh3d(meshes.add(Cuboid::new(2.0, 1.0, 3.0))),
-    //     MeshMaterial3d(materials.add(StandardMaterial {
-    //         base_color_texture: Some(camo_texture.clone()),
-    //         perceptual_roughness: 0.8,
-    //         metallic: 0.1, 
-    //         ..default()
-    //     })),
-    //     Transform::from_xyz(0.0, 0.5, 0.0),
-    //     RigidBody::Dynamic,
-    //     Collider::cuboid(1.0,0.5,1.5),
-    //     ExternalForce::default(),
-    //     Tank
-    // )).id();
-
-    // let turret_entity = commands.spawn((
-    //     Mesh3d(meshes.add(Cuboid::new(1.0, 0.5, 1.0))),
-    //     MeshMaterial3d(materials.add(StandardMaterial {
-    //         base_color_texture: Some(camo_texture.clone()),
-    //         perceptual_roughness: 0.8,
-    //         metallic: 0.1, 
-    //         ..default()
-    //     })),
-    //     Transform::from_xyz(0.0, 0.75, 0.0),
-    //    // RigidBody::Dynamic,
-    //     Collider::cuboid(0.5,0.25,0.5),
-    //     ExternalForce::default()
-    // )).id();
-
-    // let gun_entity = commands.spawn((
-    //     Mesh3d(meshes.add(Cylinder::new(0.1, 3.0))),
-    //     MeshMaterial3d(materials.add(StandardMaterial {
-    //         base_color_texture: Some(camo_texture.clone()),
-    //         perceptual_roughness: 0.8,
-    //         metallic: 0.1, 
-    //         ..default()
-    //     })),
-    //     Transform::from_xyz(0.0, 0.0, -2.0).with_rotation(Quat::from_rotation_x(90.0_f32.to_radians())),
-    //     //RigidBody::Dynamic,
-    //     Collider::cylinder(1.5,0.1),
-    //     ExternalForce::default()
-    // )).id();
-
-    // commands.entity(body_entity).add_child(turret_entity);
-
-    // commands.entity(turret_entity).add_child(gun_entity);
-
+    //Tank
     let tank_parent = commands.spawn((
         Transform::from_xyz(0.0, 1.0, 0.0),
         RigidBody::Dynamic,
         Collider::cuboid(1.1,1.0,1.3),
+        InheritedVisibility::VISIBLE,
         Tank
     )).id();
 
@@ -120,13 +115,14 @@ fn setup(
 
     //Camera
     commands.spawn((
-        Camera3d {
+        Transform::from_xyz(-15.0, 15.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        PanOrbitCamera {
+            enabled: false,
             ..default()
         },
-        Transform::from_xyz(-15.0, 15.0, -15.0).looking_at(Vec3::ZERO, Vec3::Y),
-        GlobalTransform::default(),
-        Camera
+        Camera::default(),
     ));
+
 }
 
 fn tank_movement(
@@ -137,21 +133,23 @@ fn tank_movement(
     let mut transform = query.single_mut();
     let mut direction = 0.0;
     let mut rotation = 0.0;
+    let drive_velocity = 0.8;
+    let rotation_velocity = 0.5;
 
     if keyboard.pressed(KeyCode::KeyW) {
-        direction += 1.0;
+        direction += drive_velocity;
     }
 
     if keyboard.pressed(KeyCode::KeyS) {
-        direction -= 1.0;
+        direction -= drive_velocity;
     }
 
     if keyboard.pressed(KeyCode::KeyA) {
-        rotation -= 1.0;
+        rotation += rotation_velocity;
     }
 
     if keyboard.pressed(KeyCode::KeyD) {
-        rotation += 1.0;
+        rotation -= rotation_velocity;
     }
 
     let rotation_speed = 2.0;
@@ -164,12 +162,69 @@ fn tank_movement(
 
 fn camera_follow(
     tank_query: Query<&Transform, (With<Tank>, Without<Camera>)>,
-    mut cam_query: Query<&mut Transform, (With<Camera>, Without<Tank>)>
+    mouse: Res<ButtonInput<MouseButton>>,
+    mut query: Query<(&mut Camera, &mut PanOrbitCamera)>
 )   {
-    let tank_transform = tank_query.single();
-    let mut cam_transfrom = cam_query.single_mut();
 
-    let behind = tank_transform.translation - tank_transform.forward() * 10.0 + Vec3::Y * 5.0;
-    cam_transfrom.translation = behind;
-    cam_transfrom.look_at(tank_transform.translation, Vec3::Y);
+    for (mut camera, mut orbit_camera) in &mut query
+    {
+        if mouse.just_pressed(MouseButton::Middle) || camera.use_follow_cam {
+
+            let tank_transform = tank_query.single();
+
+            let focus = tank_transform.translation;
+        
+            let direction = (focus - tank_transform.forward() * 15.0 + Vec3::Y * 10.0) - focus;
+
+            orbit_camera.target_focus = focus;
+            orbit_camera.target_radius = direction.length();
+            orbit_camera.target_yaw = direction.x.atan2(direction.z);
+            orbit_camera.target_pitch = (direction.y / direction.length()).asin();
+
+            if mouse.just_pressed(MouseButton::Middle) {
+                camera.use_follow_cam = !camera.use_follow_cam;
+                orbit_camera.enabled = !camera.use_follow_cam;
+            }
+
+            if !camera.use_follow_cam {
+                orbit_camera.enabled = true;
+           }
+        }      
+    }
+}
+
+fn shoot_projectile(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut commands: Commands,
+    query: Query<&Transform, With<Tank>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+){
+    if keyboard.just_pressed(KeyCode::Space) {
+        if let Ok(tank_transform) = query.get_single() {
+
+            let rotation = tank_transform.rotation;
+            let direction = tank_transform.forward();
+
+            commands.spawn((
+                Transform::from_translation(tank_transform.translation + (direction * 2.0)).with_rotation(rotation),
+                Mesh3d(meshes.add(Cylinder::default())),
+                MeshMaterial3d(materials.add(StandardMaterial {base_color: BLUE.into(), ..Default::default()})),
+                Projectile,
+                Collider::cylinder(0.5, 0.5)
+            ));
+        }
+    }
+}
+
+fn move_projectile(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Projectile>>
+) {
+    let projectile_velocity = 6.0;
+
+    for mut transform in query.iter_mut() {
+        let forward = transform.forward(); // This gives you the +Z direction
+        transform.translation += forward * projectile_velocity * time.delta_secs();
+    }
 }
